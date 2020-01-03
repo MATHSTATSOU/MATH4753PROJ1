b5122d0f4950627d6e5ed4cef44adefb474bd8ca
Wayne Stewart <wayne.s.stewart@ou.edu>
2020-01-03 12:11:55 -0600

    Initial file list

---
+++ R/myf.R
@@ -0,0 +1,15 @@
+#' Square function
+#'
+#' Takes a vector and returns a vector of squared components
+#'
+#' The function is an elementary example of function creation and can be massively enhanced
+#' @param x
+#'
+#' @return a Vector of squared components
+#' @export
+#'
+#' @examples
+#' x=1:30; myf(x)
+myf=function(x){
+  x^2
+}
